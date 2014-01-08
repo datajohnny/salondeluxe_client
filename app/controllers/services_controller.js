a58ea29ee711d80719bdbf4fefe144afab0985c3
Johnny <jmejias17@gmail.com>
2014-01-07 20:19:47 -0400

    Working hack for service animation

--- app/controllers/services_controller.js
+++ app/controllers/services_controller.js
@@ -1,21 +1,24 @@
 App.ServicesController = Ember.ArrayController.extend({
   sortProperties: ['name'],
   filtered_categories: [],
+  self: this,
 
   actions: {
     showAll: function() {
       this.set('filtered_categories', this.get('content'));
-      this.$('dd.active').removeClass('active');
-      this.$('#all-services').addClass('active');
+      // view manipulation
+      self.$('dd.active').removeClass('active');
+      self.$('#all-services').addClass('active');
     },
 
     hideOthers: function(category) {
       var all_categories = this.get('content');
       var filtered_categories = this.get('content').filterBy('category', category);
       this.set('filtered_categories', filtered_categories);
+      // view manipulation
       var category_filter_id = "#" + category + "-filter";
-      this.$('dd.active').removeClass('active');
-      this.$(category_filter_id).addClass('active');
+      self.$('dd.active').removeClass('active');
+      self.$(category_filter_id).addClass('active');
     }
   }
 });
