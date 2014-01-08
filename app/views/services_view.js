8a46318fa888c14f022b9f31fbbdb92b40ef9b66
Johnny <jmejias17@gmail.com>
2014-01-07 21:49:26 -0400

    Service items appear on screen

--- app/views/services_view.js
+++ app/views/services_view.js
@@ -1,6 +1,10 @@
 App.ServicesView = Ember.CollectionView.extend({
   classNames: ['services', 'row'],
+  contentBinding: 'controller',
+
   itemViewClass: Ember.View.extend({
+    templateName: 'services/item',
+
     didInsertElement: function() {
       console.log("inserted");
     },
